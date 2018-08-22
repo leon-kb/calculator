7ce8782c244a7b74563e8ab03bd5f2cf18cefaec
lwi020 <leon.dewit@kb.nl>
2018-08-22 11:18:33 +0200

    Added Redis caching.

--- src/main/java/nl/leon/calculator/Calculator.java
+++ src/main/java/nl/leon/calculator/Calculator.java
@@ -1,13 +1,13 @@
 package nl.leon.calculator;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/**
- * Main Service Class.
- */
+/** Calculator logic */
 @Service
 public class Calculator {
-    int sum(int a, int b) {
+    @Cacheable("sum")
+    public int sum(int a, int b) {
         return a + b;
     }
 }
