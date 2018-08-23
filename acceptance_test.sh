ed2154d50c494aaf64877b0a02a66d7453fc8568
Leon <leon.dewit@ziggo.nl>
2018-08-23 11:57:42 +0200

    added automated acceptancetest

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
-test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
+./gradlew acceptanceTest -Dcalculator.url=http://localhost:$CALCULATOR_PORT
+#test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
